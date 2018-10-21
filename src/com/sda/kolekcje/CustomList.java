7ecb66c66e24eaf0750dc6c2b49c299f0e9b6465
tomlach <tlachawczak@gmail.com>
2018-10-21 12:54:34 +0200

    Zapisane zmiany w projekcie

--- src/com/sda/kolekcje/CustomList.java
+++ src/com/sda/kolekcje/CustomList.java
@@ -3,6 +3,43 @@ package com.sda.kolekcje;
 //Iimplementacja wlasnej arraylisty
 public class CustomList<E> {
     private int size = 0;
-    private final int DEFAULT CAPACITY = 10;
+    private final int DEFAULT_CAPACITY = 10;
     private Object elements[];
+
+    public CustomList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size++] = e;
+    }
+
+    public void resize() {
+        int newSize = elements.length * 2;
+        Object tmp[] = new Object[newSize];
+        for(int i = 0; i<elements.length; i++) {
+            tmp[i] = elements[i];
+        }
+        elements = tmp;
+
+        //inne rozwiazanie
+        //elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public E get(int i){
+        if(i>=size || i<0){
+            throw new IndexOutOfBoundsException("Nie ma takiego elementu");
+        }
+        return (E) elements[i];
+    }
+    public int size(){
+        return size;
+    }
+
+    public void remove(int index){
+        //miejsce w tablicy, ktore usuwa
+    }
 }
--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        CustomList<String> lista = new CustomList<>();
+        lista.add("Jan1");
+        lista.add("Jan2");
+        lista.add("Jan3");
+        lista.add("Jan4");
+        lista.add("Jan5");
+        lista.add("Jan6");
+        lista.add("Jan7");
+        lista.add("Jan8");
+        lista.add("Jan9");
+        lista.add("Jan10");
+        lista.add("Jan11");
+        lista.add("Jan12");
+        lista.add("Jan13");
+        lista.add("Jan14");
+
+//lista.get(-2);
+        for(int i=0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
     }
 }
