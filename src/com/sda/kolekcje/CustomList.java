04e21279c31b52303a6fcb26f9cfb0c22ae5c861
tomlach <tlachawczak@gmail.com>
2018-10-21 12:03:03 +0200

    inicjalizacja

---
+++ src/com/sda/kolekcje/CustomList.java
@@ -0,0 +1,8 @@
+package com.sda.kolekcje;
+
+//Iimplementacja wlasnej arraylisty
+public class CustomList<E> {
+    private int size = 0;
+    private final int DEFAULT CAPACITY = 10;
+    private Object elements[];
+}
